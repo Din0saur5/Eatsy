e2971acccedce877d308d423ae5489aa6cb149c0
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-12-18 18:18:44 -0500

    front end prod testing

--- client/src/components/NavbarUser.jsx
+++ client/src/components/NavbarUser.jsx
@@ -19,7 +19,7 @@ const NavbarUser = () => {
             const { error } = await supabase.auth.signOut()
             if (error) throw error
             sessionStorage.clear()
-            location.reload()
+            navigate('/')
             return 
             
         } catch (error) {
