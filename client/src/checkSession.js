25cc25fbb9da302eda1dcca4285fada829d74560
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-12-19 14:41:47 -0500

    it will atleast load

--- client/src/checkSession.js
+++ client/src/checkSession.js
@@ -2,15 +2,15 @@
 
 const checkSession = async () => {
 const server = import.meta.env.VITE_BACK_END_SERVE
-
+    
       // auto-login
     const user = await fetch(`${server}/check_session`).then((r) => {
         if (r.ok) {
-          r.json().then((data) => data);
+          r.json().then((data) => {return data});
         }
       });
     
-   return user
+   return user ? user: null
     
 }
 export default checkSession;
