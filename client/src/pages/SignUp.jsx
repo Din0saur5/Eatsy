0c2bb4ecf9d73bb4637df237f5820418e84c6cc0
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-12-19 01:07:30 -0500

    sign up testing

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -63,7 +63,7 @@ const SignUp = () => {
     })
     .then(response => response.json() )
     .then(data => {
-      console.log(data) // Handle the response data
+      return data // Handle the response data
     })
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error); // Handle any errors
@@ -80,22 +80,17 @@ const SignUp = () => {
         password:formInfo.password,
         disable_email_confirmation: true,
     })
-     .then(()=>{
+     
       postPublicUser(data) 
-      handleLogin()
+      await handleLogin()
       setFormInfo({
         email: '',
         password: '',
         username: '',
         first_name: '',
-        last_name: '',
-        
-       
-      });
-      
-      location.reload()
-    }
-      )
+        last_name: '',                 
+    })
+    navigate('/dashboard')
 
     if (error) throw error;
     
@@ -132,7 +127,7 @@ const SignUp = () => {
   </div>
   <div className="relative z-0 w-full mb-5 group">
       <input type="username3" name='username' onChange={handleChange} value={formInfo.username} id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " required />
-      <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+      <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
   </div>
  
   <div className="grid md:grid-cols-2 md:gap-6">
