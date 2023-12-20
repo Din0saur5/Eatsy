8f81f96d323f43bced084d6c87ab8b16800a08b9
Daniel Hackworth <dan.hackworth.515@gmail.com>
2023-12-20 18:20:36 -0500

    the craziest form

--- client/src/components/CreateRecipeForm.jsx
+++ client/src/components/CreateRecipeForm.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
-import { Accordion } from 'flowbite-react';
-import React, { useState } from 'react';
+import { Accordion, Button, Select } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
+import AutocompleteInput from './AutoCompleteCuisine';
 
-const CreateRecipeForm = ({userData}) => {
+const CreateRecipeForm = ({userData, handleClose}) => {
     console.log(userData)
   const [formData, setFormData] = useState({
     name: '',
     image: '',
     description: '',
-    steps: '',
+    steps: [''],
     is_draft: false,
-    tags: '',
+    tags: [''],
     cuisine: '',
     meal_type: '',
     dish_type: '',
@@ -18,6 +19,16 @@ const CreateRecipeForm = ({userData}) => {
     user_id: userData.id, 
     ingredients: [{ text: '', food: '', quantity: '', unit: '' }]
   });
+
+  const [isFormValid, setIsFormValid] = useState(false); 
+
+  useEffect(() => {
+    // Update isFormValid based on formData
+    const isFormDataValid = formData.name && formData.image && formData.description 
+                            && formData.steps.every(step => step.trim() !== '') 
+                            && formData.tags;
+    setIsFormValid(isFormDataValid);
+  }, [formData]);
 
   const handleChange = (e) => {
     if (e.target.name.startsWith('ingredients')) {
@@ -36,6 +47,17 @@ const CreateRecipeForm = ({userData}) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const processedTags = formData.tags
+    .split('#')
+    .filter(tag => tag.trim() !== '') // Remove empty tags
+    .map(tag => tag.trim().replace(/_/g, ' ').replace(/,/g, '')); // Replace underscores with spaces and remove commas
+
+  // Create the final form data object for submission
+    const finalFormData = {
+    ...formData,
+    tags: processedTags
+  };
+  console.log(finalFormData);
     const server = import.meta.env.VITE_BACK_END_SERVE
     try {
       const response = await fetch(`${server}/create_recipe`, {
@@ -44,7 +66,7 @@ const CreateRecipeForm = ({userData}) => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(finalFormData)
       });
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -62,105 +84,95 @@ const CreateRecipeForm = ({userData}) => {
       ingredients: [...formData.ingredients, { text: '', food: '', quantity: '', unit: '' }]
     });
   };
+  const handleChangeStep = (index, value) => {
+    const newSteps = formData.steps.map((step, i) => (
+      i === index ? value : step
+    ));
+    setFormData({ ...formData, steps: newSteps });
+  };
+
+  const handleAddStep = () => {
+    setFormData({ ...formData, steps: [...formData.steps, ''] });
+  };
+  
 
   return (
-    <form onSubmit={handleSubmit}>
-      
-      <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
-      <input type="text" name="image" value={formData.image} onChange={handleChange} placeholder="Image URL" />
-      <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Description" />
-      <textarea name="steps" value={formData.steps} onChange={handleChange} placeholder="Steps" />
-      
-      <input type="text" name="tags" value={formData.tags} onChange={handleChange} placeholder="Tags" />
-      <input type="text" name="cuisine" value={formData.cuisine} onChange={handleChange} placeholder="Cuisine" />
-      <input type="text" name="meal_type" value={formData.meal_type} onChange={handleChange} placeholder="Meal Type" />
-      <input type="text" name="dish_type" value={formData.dish_type} onChange={handleChange} placeholder="Dish Type" />
-      <input type="text" name="time" value={formData.time} onChange={handleChange} placeholder="Time" />
-      <input type="text" name="user_id" value={formData.user_id} onChange={handleChange} placeholder="User ID" />
+    <form className="h-96 overflow-y-auto" onSubmit={handleSubmit}>
+        <Accordion>
+            <Accordion.Panel>
+                <Accordion.Title>Basic Info?</Accordion.Title>
+                <Accordion.Content>
+                <input className='rounded-lg mb-2 mr-1' type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name"  required/>
+                <input className='rounded-lg mb-2 mr-1' type="text" name="image" value={formData.image} onChange={handleChange} placeholder="Image URL"  required/>
+                <input className='rounded-lg mb-2 mr-1' type="number" name="time" value={formData.time} onChange={handleChange} placeholder="Est. Cook Time" required />
+                <input className='rounded-lg mb-2 mr-1' type="text" name="meal_type" value={formData.meal_type} onChange={handleChange} placeholder="Meal Type"  required/>
+                <AutocompleteInput type="text" name="cuisine" value={formData.cuisine} onChange={handleChange} placeholder="Cuisine" required />
+                <div className="max-w-md">
+                <div className="mb-2 block">
+                    </div>
+                <Select name="dish_type" value={formData.dish_type} onChange={handleChange}  required> 
+                    <option className='text-white hover:text-blue-400'>Dish Type</option>
+                    <option>Breakfast</option>
+                    <option>Lunch/Dinner</option>
+                    <option>Teatime</option>
+                    <option>Snack</option>
+                </Select>
+                </div>            
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>Description and Tags?</Accordion.Title>
+                <Accordion.Content>
+                    <div className='flex flex-col'>
+                <textarea className='rounded-lg mb-2 mr-1' name="description" value={formData.description} onChange={handleChange} placeholder="Description"  required/>
+                <label className='text-gray-500' htmlFor='tags'>add tags for better searchability (format: #veggie_soup) </label>
+                <input className='rounded-lg mb-2 mr-1' type="text" name="tags" value={formData.tags} onChange={handleChange} placeholder="Tags"  required />
+                </div>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>Ingredients?</Accordion.Title>
+                <Accordion.Content>
+                
+                {formData.ingredients.map((ingredient, index) => (
+                <div key={index}>
+                <input className='rounded-lg mb-2 mr-1' type="text" name={`ingredients[${index}].text`} data-index={index} data-field="text" value={ingredient.text} onChange={handleChange} placeholder="Extra Details"  required/>
+                <input className='rounded-lg mb-2 mr-1' type="text" name={`ingredients[${index}].food`} data-index={index} data-field="food" value={ingredient.food} onChange={handleChange} placeholder="Food"  required/>
+                <input className='rounded-lg mb-2 mr-1' type="text" name={`ingredients[${index}].quantity`} data-index={index} data-field="quantity" value={ingredient.quantity} onChange={handleChange} placeholder="Quantity"  required/>
+                <input className='rounded-lg mb-2 mr-1' type="text" name={`ingredients[${index}].unit`} data-index={index} data-field="unit" value={ingredient.unit} onChange={handleChange} placeholder="Unit"  required/>
+                </div>
+            ))}
+            <button className='border bg-blue-400  px-2 p-0.5 rounded-lg mb-2 mr-1' type="button" onClick={handleAddIngredient}>Add Ingredient</button>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>Steps?</Accordion.Title>
+                <Accordion.Content >
+                {formData.steps.map((step, index) => (
+                        <div key={index}>
+                        <input 
+                            type="text" 
+                            value={step} 
+                            onChange={(e) => handleChangeStep(index, e.target.value)} 
+                            placeholder={`Step ${index + 1}`} 
+                            className="border border-gray-400 rounded p-2 my-1"
+                            required
+                        />
+                        </div>
+                    ))}
+                   
+                    <button  className='border  bg-blue-400  px-2 p-0.5 rounded-lg mb-2 mr-1' type="button" onClick={handleAddStep}>Add Step</button>
+                </Accordion.Content>
+            </Accordion.Panel>
+            </Accordion>
 
-      {/* Dynamic ingredient fields */}
-      {formData.ingredients.map((ingredient, index) => (
-        <div key={index}>
-          <input type="text" name={`ingredients[${index}].text`} data-index={index} data-field="text" value={ingredient.text} onChange={handleChange} placeholder="Ingredient Text" />
-          <input type="text" name={`ingredients[${index}].food`} data-index={index} data-field="food" value={ingredient.food} onChange={handleChange} placeholder="Food" />
-          <input type="text" name={`ingredients[${index}].quantity`} data-index={index} data-field="quantity" value={ingredient.quantity} onChange={handleChange} placeholder="Quantity" />
-          <input type="text" name={`ingredients[${index}].unit`} data-index={index} data-field="unit" value={ingredient.unit} onChange={handleChange} placeholder="Unit" />
+        <div className='flex flex-row justify-between'> 
+            <Button  disabled={!isFormValid} gradientMonochrome="success"  className={`mt-4 px-4 py-2 ${isFormValid ? '': 'bg-gray-500 text-gray-200'}` } type="submit">Create Recipe</Button>
+            <Button gradientMonochrome="failure" onClick={handleClose} className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">Cancel</Button>
         </div>
-      ))}
-      <button type="button" onClick={handleAddIngredient}>Add Ingredient</button>
-
-      <button type="submit">Create Recipe</button>
     </form>
+    
   );
 };
 
 export default CreateRecipeForm; 
-
-<Accordion>
-      <Accordion.Panel>
-        <Accordion.Title>What is Flowbite?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
-            dropdowns, modals, navbars, and more.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out this guide to learn how to&nbsp;
-            <a
-              href="https://flowbite.com/docs/getting-started/introduction/"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-            >
-              get started&nbsp;
-            </a>
-            and start developing websites even faster with components on top of Tailwind CSS.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>Is there a Figma file available?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-            has a design equivalent in our Figma file.
-          </p>
-          <p className="text-gray-500 dark:text-gray-400">
-            Check out the
-            <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-              Figma design system
-            </a>
-            based on the utility classes from Tailwind CSS and components from Flowbite.
-          </p>
-        </Accordion.Content>
-      </Accordion.Panel>
-      <Accordion.Panel>
-        <Accordion.Title>What are the differences between Flowbite and Tailwind UI?</Accordion.Title>
-        <Accordion.Content>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-            Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-            components, whereas Tailwind UI offers sections of pages.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">
-            However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-            technical reason stopping you from using the best of two worlds.
-          </p>
-          <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-          <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-            <li>
-              <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                Flowbite Pro
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://tailwindui.com/"
-                rel="nofollow"
-                className="text-cyan-600 hover:underline dark:text-cyan-500"
-              >
-                Tailwind UI
-              </a>
-            </li>
-          </ul>
-        </Accordion.Content>
-      </Accordion.Panel>
-    </Accordion>
