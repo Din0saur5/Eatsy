22e6efc7b985cee54f7acfaa653a6eff829354d5
Austin Byrd <byrda05@gmail.com>
2023-12-20 21:50:07 -0600

    updated dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,108 +1,113 @@
 // Dashboard.jsx
-import { Button } from 'flowbite-react';
-import { useEffect, useState } from "react";
-import { redirect, useOutletContext } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useOutletContext } from "react-router-dom";
 import ToggleSwitch from '../components/ToggleSwitch';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import RecipeCard from '../components/RecipeCard';
 import CreateRecipeButton from '../components/CreateRecipeButton';
 import CreateRecipe from '../components/CreateRecipe';
-
-
-const server = import.meta.env.VITE_BACK_END_SERVE
-
-
-
+import Button from 'flowbite-react';
 
 const Dashboard = () => {
-
   const [userData, setUserData] = useOutletContext();
   const [isToggled, setIsToggled] = useState(true); // true for 'My Recipes', false for 'Favorites'
-  const [favorites, setFavorites] = useState([])
-  const [ userRecipes, setUserRecipes] = useState([])
-const [isModalOpen, setIsModalOpen] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [userRecipes, setUserRecipes] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loadMoreCount, setLoadMoreCount] = useState(6);
+  const [hasMoreRecipes, setHasMoreRecipes] = useState(true);
+
   const fetchFavorites = async () => {
-    const server = import.meta.env.VITE_BACK_END_SERVE
+    const server = import.meta.env.VITE_BACK_END_SERVE;
     try {
-        
-        const response = await fetch(`${server}/favorites/${userData.id}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        setFavorites(data);
-    } catch (error) {
-        console.error('Error fetching recipes:', error);
-    }
-};
-const fetchMyRecipes = async () => {
-  const server = import.meta.env.VITE_BACK_END_SERVE
-  try {
-      
-      const response = await fetch(`${server}/recipes/created_by/${userData.id}`);
+      const response = await fetch(`${server}/favorites/${userData.id}`);
       if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
-      setUserRecipes(data);
-      console.log(userRecipes)
-  } catch (error) {
+      setFavorites(data);
+    } catch (error) {
+      console.error('Error fetching favorites:', error);
+    }
+  };
+
+  const fetchMyRecipes = async (limit = 6, offset = 0) => {
+    const server = import.meta.env.VITE_BACK_END_SERVER;
+    try {
+      const url = `${server}/recipes/created_by/${userData.id}?limit=${limit}&offset=${offset}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (data.length < limit) {
+        setHasMoreRecipes(false);
+      }
+      setUserRecipes(prevRecipes => [...prevRecipes, ...data]);
+    } catch (error) {
       console.error('Error fetching recipes:', error);
-  }
-};
-  useEffect(()=>{
-      fetchFavorites()
-      fetchMyRecipes()
-  }, [])
+    }
+  };
+
+  useEffect(() => {
+    fetchFavorites();
+    fetchMyRecipes();
+  }, []);
 
   const toggle = () => {
     setIsToggled(!isToggled);
   };
+
   useEffect(() => {
     AOS.init({
       duration: 1000, // values from 0 to 3000, with step 50ms
       once: true, // whether animation should happen only once - while scrolling down
     });
   }, []);
-  const RecipeList = ({list}) => {
+
+  const RecipeList = ({ list }) => {
     return (
       <>
-        {list.map((recipe, index)=>{
-         return <RecipeCard recipe={recipe} key={index} />
-
+        {list.map((recipe, index) => {
+          return <RecipeCard recipe={recipe} key={index} />
         })}
-       
       </>
     );
   };
- 
- console.log(userData)
- return (
-  <>
 
-    <div className=' flex justify-center items-center bg-bg7' >
-      
-      <div className='w-4/5 bg-beige dark:bg-brown'>
-        
-  <div className='m-3'>
-  <CreateRecipeButton setIsModalOpen={setIsModalOpen}/>
-  </div>
-<div className=" flex items-center justify-center py-4 md:py-8 flex-wrap">
-  <ToggleSwitch toggle={toggle} isToggled={isToggled}/>
-</div>
-<div className=" sm:ml-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4">
-  {toggle ? (
-    <RecipeList list={userRecipes}/>
-  ):(
-    <RecipeList list={[]}/>
-  )}
-  
-</div>
-</div>
-    </div>
-    <CreateRecipe userData={userData} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+  const loadMore = () => {
+    const newRecipesToLoad = 6;
+    const currentOffset = userRecipes.length;
+    setLoadMoreCount(prevCount => prevCount + newRecipesToLoad);
+    fetchMyRecipes(newRecipesToLoad, currentOffset);
+  };
 
+  return (
+    <>
+      <div className=' flex justify-center items-center bg-bg7'>
+        <div className='w-4/5 bg-beige dark:bg-brown'>
+          <div className='m-3'>
+            <CreateRecipeButton setIsModalOpen={setIsModalOpen} />
+          </div>
+          <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+            <ToggleSwitch toggle={toggle} isToggled={isToggled} />
+          </div>
+          <div className="sm:ml-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {isToggled ? (
+              <RecipeList list={userRecipes} />
+            ) : (
+              <RecipeList list={favorites} />
+            )}
+          </div>
+          {isToggled && hasMoreRecipes && (
+            <div className="text-center mt-4">
+              <Button onClick={loadMore}>Load More Recipes</Button>
+            </div>
+          )}
+        </div>
+      </div>
+      <CreateRecipe userData={userData} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </>
   );
 };
