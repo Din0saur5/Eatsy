5d5250e1520a4ad2288928dc7e9e18848004d8a9
Austin Byrd <byrda05@gmail.com>
2023-12-20 23:11:04 -0600

    added recipe by name endpoint

--- server/app.py
+++ server/app.py
@@ -447,6 +447,30 @@ class FavoriteRecipe(Resource):
 
 api.add_resource(FavoriteRecipe, '/favorites/<uuid:rec_id>/<uuid:user_id>')
 
+class RecipeNames(Resource):
+    def get(self):
+        # Query only the names of the recipes from the database
+        recipes = Recipe.query.with_entities(Recipe.name).all()
+        
+        # Create a dictionary to hold the recipes sorted by first letter
+        sorted_recipes = {}
+        for recipe in recipes:
+            # Get the first letter of the recipe name
+            first_letter = recipe.name[0].upper()
+            if first_letter not in sorted_recipes:
+                sorted_recipes[first_letter] = []
+            sorted_recipes[first_letter].append(recipe.name)
+        
+        # Sort the recipes under each letter
+        for letter in sorted_recipes:
+            sorted_recipes[letter].sort()
+
+        return sorted_recipes
+
+# Add the resource to the API
+api.add_resource(RecipeNames, '/recipes/names')
+
+
 port = os.getenv('SERVER_PORT')
 debug = os.getenv('SERVER_DEBUG')
 host = os.getenv('SERVER_HOST')
