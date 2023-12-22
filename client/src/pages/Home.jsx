3dede2121eb9a2e7f6300b6bd40e783dcffff83c
Austin Byrd <byrda05@gmail.com>
2023-12-21 18:42:51 -0600

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import '../App.css';
-import Navbar from '../components/Navbar';
-
-
-
-const Home = () => {
-  
-  
+const HomePage = () => {
   return (
-    <>
-    
-    <div className='bg-background5 bg-cover' >
-      <div className="flex justify-center items-center h-screen">
-    
-    </div>
-    </div>
-    </>
-  )
+    <div>
+      {/* Hero Section */}
+      <section className="hero bg-background1 bg-cover text-center py-20">
+        <h1 className="text-4xl font-bold mb-4">Explore the World Through Flavors</h1>
+        <p className="text-xl mb-6">Discover new recipes and culinary inspirations daily.</p>
+        <Link to="/search" className="btn btn-primary">Find Recipes</Link>
+      </section>
 
-}
+      {/* Featured Recipes */}
+      <section className="featured-recipes py-20">
+        <h2 className="text-3xl text-center font-bold mb-6">Featured Recipes</h2>
+        {/* Recipes grid or carousel goes here */}
+      </section>
 
-export default Home
+      {/* Categories Section */}
+      <section className="categories py-20 bg-gray-100">
+        <h2 className="text-3xl text-center font-bold mb-6">Browse by Category</h2>
+        {/* Category cards go here */}
+      </section>
+
+      {/* Testimonials Section */}
+      <section className="testimonials py-20">
+        <h2 className="text-3xl text-center font-bold mb-6">What Our Users Say</h2>
+        {/* Testimonials carousel goes here */}
+      </section>
+
+      {/* Blog Highlights */}
+      <section className="blog-highlights py-20 bg-gray-100">
+        <h2 className="text-3xl text-center font-bold mb-6">From Our Blog</h2>
+        {/* Blog post previews go here */}
+      </section>
+
+      {/* Call to Action */}
+      <section className="cta py-20">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold mb-4">Join Our Foodie Family</h2>
+          <p className="mb-6">Get the latest recipes and tips directly to your inbox.</p>
+          <Link to="/signup" className="btn btn-primary">Sign Up</Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
